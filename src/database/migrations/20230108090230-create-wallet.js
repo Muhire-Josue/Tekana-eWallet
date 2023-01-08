3b7d18680c181af5ecd30650ec99e702a135c6c1
Muhire-Josue <rutayisire.josue09@gmail.com>
2023-01-08 18:20:09 +0200

    Indexing tables

--- src/database/migrations/20230108090230-create-wallet.js
+++ src/database/migrations/20230108090230-create-wallet.js
@@ -1,27 +1,31 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('wallet', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      user_id: {
-        type: Sequelize.INTEGER,
-      },
-      balance: {
-        type: Sequelize.DOUBLE,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+    return queryInterface
+      .createTable('wallet', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        user_id: {
+          type: Sequelize.INTEGER,
+        },
+        balance: {
+          type: Sequelize.DOUBLE,
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() =>
+        queryInterface.addIndex('wallet', ['id', 'user_id', 'balance', 'created_at', 'updated_at']),
+      );
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('wallet');
